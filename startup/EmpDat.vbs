c3269052ac4c0aab84712c21c98df5541b67c35f
Elliot Elison <elliotjelison@gmail.com>
2022-02-22 15:37:19 -0700

    changed startup to use an exe file and included a way to regenerate the exe

--- startup/EmpDat.vbs
+++
@@ -1,3 +0,0 @@
-Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "requirements.bat" & Chr(34), 0
-Set WshShell = Nothing
--- startup/requirements.bat
+++ startup/requirements.bat
@@ -1,3 +1,36 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFB5YQA2NAE+1BaAR7ebv/NbGsU4VW+0zRKzoiODbcNwn71fpRYQi3H9ZjPcqHhRWajelajMVun1Hone5O8ibvDDyR0mF6gU5GGoU
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFB5YQA2NAE+1BaAR7ebv/NbGsU4VW+0zRKzoiODbcNwn71fpRYQi3H9ZjPcfGBpKage7Uio5uUpDoiqAL8L8
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
 
 goto :PYTHON_CHECK
@@ -20,5 +53,4 @@ pip install pillow
 goto :RUN_PROGRAM
 
 :RUN_PROGRAM
-cd ..
 python ./main.py
