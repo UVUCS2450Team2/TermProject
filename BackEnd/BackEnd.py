bae15f88df2be45df09c418484ee4dc6626f06d7
michaeldouglas <michael_gcm@hotmail.com>
2022-02-22 09:35:31 -0700

    Inital setup of backend

---
+++ BackEnd/BackEnd.py
@@ -0,0 +1,15 @@
+"""
+    This class is the backend implementation of the project.
+
+    It encompasses the following :
+
+    Read in a CSV file and file in a data structure array
+    Manage a data structure array of empData
+    Export a CSV file with formatted data from the data structure array
+
+"""
+
+class BackendImplementation:
+    def __init__(self):
+        pass
+
